60eaa9302d5017f6a3ce17c1c7fab45364de61be
Abhinav Kumar <abhinav.kumar@ideaboard.xyz>
2018-07-23 07:41:40 +0530

    debounce added on onchange event

--- src/OmniBox.js
+++ src/OmniBox.js
@@ -1,59 +1,53 @@
 import React, { Component } from 'react';
 import { TextInput } from 'react-native';
+import PropTypes from 'prop-types';
+
 import TodoModel from './TodoModel';
 import TodoService from './TodoService';
 import Utils from './Utils';
+import { debounce } from './helper/Util';
 
 class OmniBox extends Component {
   constructor(props) {
     super(props);
+    this.state = { newValue: '' };
     this.onChange = this.onChange.bind(this);
-    // this.onKeyPress = this.onKeyPress.bind(this);
     this.onSubmitEditing = this.onSubmitEditing.bind(this);
-  }
-
-  componentWillMount() {
-    this.setState({
-      newValue: '',
-    });
   }
 
   onChange(event) {
     const title = event.nativeEvent.text;
     const dataList = this.props.data.filter(item => item.title.match(new RegExp(`.*${title}.*`, 'gi')));
-
     this.setState({
       newValue: title,
     });
     this.props.updateDataList(dataList);
   }
 
-  onSubmitEditing(event) {
-    console.log( 'hello', event);
-    // if (event.nativeEvent.key === 'Enter' && this.state.newValue) {
-      const newDataItem = new TodoModel(this.state.newValue);
-
-      const dataList = this.props.data;
-      const dataItem = Utils.findTodo(newDataItem, dataList);
-      console.log('data Item', dataItem);
-      if (dataItem) {
-        Utils.move(dataList, (dataList.indexOf(dataItem)), 0);
-
-        this.setState({
-          newValue: '',
-        });
-        this.props.updateDataList(dataList);
-        return;
-      }
-
-      dataList.unshift(newDataItem);
-      TodoService.save(newDataItem);
+  onSubmitEditing() {
+    if (this.state.newValue === '') {
+      return;
+    }
+    const newDataItem = new TodoModel(this.state.newValue);
+    const dataList = this.props.data;
+    const dataItem = Utils.findTodo(newDataItem, dataList);
+    if (dataItem) {
+      Utils.move(dataList, (dataList.indexOf(dataItem)), 0);
 
       this.setState({
         newValue: '',
       });
       this.props.updateDataList(dataList);
-    // }
+      return;
+    }
+
+    dataList.unshift(newDataItem);
+    TodoService.save(newDataItem);
+
+    this.setState({
+      newValue: '',
+    });
+    this.props.updateDataList(dataList);
   }
 
   render() {
@@ -65,12 +59,29 @@ height: 36, padding: 4, marginBottom: 0, fontSize: 16, borderWidth: 1, borderCol
         placeholder="Add a todo or Search"
         blurOnSubmit={false}
         value={this.state.newValue}
-        // onKeyPress={this.onKeyPress}
-        onChange={this.onChange}
+        onChange={debounce(() => {
+            this.onChange();
+          }, 500)}
         onSubmitEditing={this.onSubmitEditing}
       />
     );
   }
 }
+
+OmniBox.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool,
+    completedAt: PropTypes.instanceOf(Date),
+    updatedAt: PropTypes.instanceOf(Date),
+  })),
+  updateDataList: PropTypes.func,
+};
+
+OmniBox.defaultProps = {
+  data: () => {},
+  updateDataList: () => {},
+};
 
 module.exports = OmniBox;
